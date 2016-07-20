5639dc6cab086ff817dd5a597e50f500d1fffd5c
Gabriel Ruiu <gabriel.ruiu@mail.com>
2016-07-20 22:12:02 +0200

    return dummy response for now

--- src/main/java/gabrielruiu/spring/boot/config/redis/RedisEnvironmentRepository.java
+++ src/main/java/gabrielruiu/spring/boot/config/redis/RedisEnvironmentRepository.java
@@ -4,10 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.config.environment.Environment;
 import org.springframework.cloud.config.server.environment.EnvironmentRepository;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.UUID;
 
 /**
  * @author Gabriel Mihai Ruiu (gabriel.ruiu@mail.com)
  */
+@Component
 public class RedisEnvironmentRepository implements EnvironmentRepository {
 
     private static final String KEY = "config";
@@ -17,6 +22,10 @@ public class RedisEnvironmentRepository implements EnvironmentRepository {
 
     @Override
     public Environment findOne(String application, String profile, String label) {
-        return null;
+        return dummyEnvironment(application, profile, label);
+    }
+
+    private Environment dummyEnvironment(String application, String profile, String label) {
+        return new Environment(application, StringUtils.commaDelimitedListToStringArray(profile),label, UUID.randomUUID().toString());
     }
 }
