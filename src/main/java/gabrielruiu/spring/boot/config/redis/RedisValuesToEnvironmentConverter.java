8c56feff59a43943a50863a61a52a3095eaf9ffc
Gabriel Ruiu <gabriel.ruiu@mail.com>
2016-07-22 23:24:08 +0200

    prepare RedisValueToEnvironmentConverter

---
+++ src/main/java/gabrielruiu/spring/boot/config/redis/RedisValuesToEnvironmentConverter.java
@@ -0,0 +1,30 @@
+package gabrielruiu.spring.boot.config.redis;
+
+import org.springframework.cloud.config.environment.Environment;
+import org.springframework.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author Gabriel Mihai Ruiu (gabriel.ruiu@mail.com)
+ */
+public class RedisValuesToEnvironmentConverter {
+
+    public Environment convert(String application, String profiles, String label, Map<String, Set<String>> values) {
+        Environment environment = new Environment(application, StringUtils.commaDelimitedListToStringArray(profiles));
+        environment.setLabel(label);
+        environment.setVersion("unset");
+        environment.
+    }
+
+    private void convert(Environment environment, Map<String, Set<String>> values) {
+        for (Map.Entry<String, Set<String>> propertyValueMap : values.entrySet()) {
+            Map<String, String> applicationProperties = new HashMap<>();
+            for (String propertyValue : propertyValueMap.getValue()) {
+                applicationProperties.put(String.format("%s.%s", propertyValueMap.getKey(), propertyValue));
+            }
+        }
+    }
+}
