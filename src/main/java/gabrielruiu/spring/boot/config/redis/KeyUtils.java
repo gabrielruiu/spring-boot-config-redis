042a93f35ca706eeaa81cf0ca76f125238b62537
Gabriel Ruiu <gabriel.ruiu@mail.com>
2016-07-23 12:21:12 +0200

    Remove unused method in KeyUtils

--- src/main/java/gabrielruiu/spring/boot/config/redis/KeyUtils.java
+++ src/main/java/gabrielruiu/spring/boot/config/redis/KeyUtils.java
@@ -7,10 +7,6 @@ class KeyUtils {
 
     public static final String GLOBAL_APPLICATION_KEY = "application";
 
-    static String globalPropertyKeys(String profile) {
-        return applicationProperties(GLOBAL_APPLICATION_KEY, profile);
-    }
-
     static String applicationProperties(String application, String profile) {
         return String.format("%s:%s:*", application, profile);
     }
