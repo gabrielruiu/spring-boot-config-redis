f697b1e5388c1eb8706c9cf504eea581447a856d
Gabriel Ruiu <gabriel.ruiu@mail.com>
2016-07-24 14:35:36 +0200

    Set default values for application name, profiles and label

--- src/main/java/gabrielruiu/spring/boot/config/redis/RedisEnvironmentRepository.java
+++ src/main/java/gabrielruiu/spring/boot/config/redis/RedisEnvironmentRepository.java
@@ -3,6 +3,7 @@ package gabrielruiu.spring.boot.config.redis;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.config.environment.Environment;
 import org.springframework.cloud.config.environment.PropertySource;
+import org.springframework.cloud.config.server.config.ConfigServerProperties;
 import org.springframework.cloud.config.server.environment.EnvironmentRepository;
 import org.springframework.stereotype.Component;
 
@@ -12,27 +13,48 @@ import org.springframework.stereotype.Component;
 @Component
 public class RedisEnvironmentRepository implements EnvironmentRepository {
 
+    private static final String DEFAULT_LABEL = "master";
+
     @Autowired
     private RedisConfigPropertySourceProvider redisConfigPropertySourceProvider;
 
+    @Autowired
+    private ConfigServerProperties configServerProperties;
+
     @Override
     public Environment findOne(String application, String profileArray, String label) {
+        if (label == null) {
+            if (configServerProperties.getDefaultLabel() != null) {
+                label = configServerProperties.getDefaultLabel();
+            } else {
+                label = DEFAULT_LABEL;
+            }
+        }
+        if (profileArray == null) {
+            profileArray = configServerProperties.getDefaultProfile();
+        }
+        if (application == null) {
+            application = configServerProperties.getDefaultApplicationName();
+        }
+
         String[] profiles = profileArray.split(",");
         Environment environment = new Environment(application, profiles);
+
         for (String profile : profiles) {
 
-            PropertySource globalPropertySource = redisConfigPropertySourceProvider.getPropertySource(KeyUtils.GLOBAL_APPLICATION_KEY, profile, label);
+            String globalApplicationName = configServerProperties.getDefaultApplicationName();
+            PropertySource globalPropertySource = redisConfigPropertySourceProvider.getPropertySource(globalApplicationName, profile, label);
             if (globalPropertySource != null) {
                 environment.add(globalPropertySource);
             }
+
 
             PropertySource applicationPropertySource = redisConfigPropertySourceProvider.getPropertySource(application, profile, label);
             if (applicationPropertySource != null) {
                 environment.add(applicationPropertySource);
             }
         }
+
         return environment;
     }
-
-
 }
