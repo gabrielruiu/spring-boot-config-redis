aec1f1b62224ef7f8c378c0bfc6f4a5f49bb8e6b
Gabriel Ruiu <gabriel.ruiu@mail.com>
2016-07-25 20:38:02 +0200

    Redis keys now also include the label

--- src/main/java/gabrielruiu/spring/boot/config/redis/KeyUtils.java
+++ src/main/java/gabrielruiu/spring/boot/config/redis/KeyUtils.java
@@ -5,11 +5,11 @@ package gabrielruiu.spring.boot.config.redis;
  */
 class KeyUtils {
 
-    static String applicationProperties(String application, String profile) {
-        return String.format("%s:%s:*", application, profile);
+    static String applicationProperties(String application, String profile, String label) {
+        return String.format("%s:%s:%s:*", application, profile, label);
     }
 
     static String extractPropertyNameNameFromKey(String application, String profile, String label, String key) {
-        return key.replace(String.format("%s:%s:", application, profile), "");
+        return key.replace(String.format("%s:%s:%s:", application, profile, label), "");
     }
 }
